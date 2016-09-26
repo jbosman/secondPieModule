50239eb8dd0c297e8b398d452ff16946bf79d6bc
Joe Bosman <joseph.a.bosman@gmail.com>
2016-09-26 11:27:47 -0400

    First push

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path')
+
+var app = express();
+
+app.use(express.static('./index.html'));
+app.use(express.static('./browser'))
+
+app.get('/', function(req,res,next){
+	res.sendFile(path.join( __dirname , './browser/index.html'))
+})
+
+app.listen(4000, function(){
+	console.log("Pie is up on 4000");
+})
